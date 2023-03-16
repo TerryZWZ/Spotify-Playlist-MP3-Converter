a729fdfe01ba2c3fbbb6ee39bde63cd0170b0fd2
Terry <terryzhou923@gmail.com>
2023-03-16 01:52:25 -0400

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import YTSearch from 'youtube-api-search';
+import axios from 'axios';
+import Spotify from 'spotify-web-api-node';
+import './App.css'
+
+// Spotify API Information
+const clientID = '5aec34d6bd124f7b8a37a71f7f8cc3c7';
+const redirectURI = window.location.protocol + '//' + window.location.host + '/callback';
+const AUTHendpoint = 'https://accounts.spotify.com/authorize';
+const responseType = 'token';
+const authURL = `${AUTHendpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`;
+localStorage.setItem('spotifyAuth', 'signedOut');
+
+// Function to get access token from URL
+function getToken(hash) {
+    let hashes = hash.split('&');
+    let accessToken = hashes[0].replace('#access_token=', '');
+    return accessToken;
+}
+
+// YouTube API key
+const APIkey = 'AIzaSyAt4WZYL5_qoikbwqlNBVpj81d0QDNt0h8';
+
+// Function to search in YouTube API for YouTube videos
+function getLinks(playlist) {
+    let links = [];
+
+    let promise = new Promise((resolve) => {
+
+        for (let x = 0; x < playlist.length; x++) {
+            let songName = playlist[x].name;
+            let artistList = [];
+
+            for (let i = 0; i < playlist[x].artists.length; i++) {
+                artistList.push(playlist[x].artists[i].name);
+            }
+
+            let artists = artistList.join(' ');
+            let searchTerm = songName + ' lyrics ' + artists;
+
+            // Actual Search
+            YTSearch({ key: APIkey, term: searchTerm, type: 'video', maxResults: 1 }, (videos) => {
+                let vid = videos[0];
+                let link = `https://www.youtube.com/watch?v=${vid.id.videoId}`;
+                links.push(link);
+
+                if (x === playlist.length - 1) {
+                    resolve(links);
+                }
+            });
+        }
+    });
+
+    return promise;
+}
+
+function App() {
+    let accessToken = getToken(window.location.hash); // Gets access token
+
+    let [input, setInputValue] = useState('');
+
+    // Search Button Function
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        localStorage.setItem('spotifyAuth', 'signedIn');
+
+        // The link must be a spotify playlist link
+        if (input.includes("open.spotify.com/playlist/")) {
+            let playlistURL = input;
+            let playlistSplitA = playlistURL.split('/')[4];
+            let playlistSplitB = playlistSplitA.split('?');
+            let playlistID = playlistSplitB[0];
+            let playlist = [];
+
+            // The playlist ID is extracted from above, and using the access token, a request is sent
+            axios.get(`https://api.spotify.com/v1/playlists/${playlistID}`, {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            })
+                .then(response => {
+                    let tracks = response.data.tracks.items;
+                    let promiseLinks = [];
+
+                    // Exctracting Names of the Songs
+                    tracks.forEach(item => {
+                        playlist.push(item.track);
+                    });
+
+                    promiseLinks = getLinks(playlist); // Finding YouTube Links
+
+                    promiseLinks.then((youtubeLinks) => {
+                        fetch(window.location.protocol + '//' + window.location.hostname + ':4000/download', {
+                            method: 'POST',
+                            body: JSON.stringify({ youtubeLinks, playlist }),
+                            headers: { 'Content-Type': 'application/json' },
+                        })
+                            .then(res => res.blob())
+                            .then(blob => {
+                                // Connecting to server.js to download files
+                                const url = window.URL.createObjectURL(blob);
+                                const link = document.createElement('a');
+                                link.href = url;
+                                link.setAttribute('download', 'music.zip');
+                                document.body.appendChild(link);
+                                link.click();
+                            })
+                            .catch(error => console.error('Error:', error));
+                    });
+                })
+                .catch(error => {
+                    console.error("Not a valid playlist");
+                });
+        }
+        else {
+            console.log("Be sure to input the playlist link")
+        }
+    };
+
+    // Spotify Authorization Check
+    let [spotifyAuth, authSpotify] = useState(false);
+    const spotifyAPI = new Spotify();
+    spotifyAPI.setAccessToken(accessToken);
+
+    spotifyAPI.getMe().then(
+        (response) => {
+            authSpotify(true);
+        },
+        (error) => {
+            console.error('Access token is not working:', error);
+        }
+    );
+
+    // Spotify Authorization Page redirect
+    const login = () => {
+        window.location.href = authURL;
+    }
+
+    return (
+        <div className="contain">
+
+            {!spotifyAuth &&
+                <button className='spotifyLogin' onClick={login}>Login to Spotify</button>
+            }
+
+            {spotifyAuth &&
+                <div className='containForm'>
+                    <h1 className='title'>Spotify Playlist MP3 Converter</h1>
+                    <h1 className='description'>Turn a Spotify Playlist into MP3 Files</h1>
+                    <form className='convertForm' onSubmit={handleSubmit}>
+                        <label>
+                            <h1 className='message'>i.e. https://open.spotify.com/playlist/...</h1>
+                            <input className='inputLink' type="text" value={input} onChange={(event) => setInputValue(event.target.value)} />
+                        </label>
+                        <br></br><br></br>
+                        <input className='submitLink' type="submit" value="Submit" />
+                    </form>
+                </div>
+            }
+
+        </div>
+    );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,90 @@
+const express = require('express'); // Routing
+const ytdl = require('ytdl-core'); // To Download YouTube Links
+const bodyParser = require('body-parser'); // For extracting data and have access to it
+const cors = require('cors'); // Enable CORS policy
+const archiver = require('archiver'); // For downloading zip files
+const fs = require('fs'); // File system operations
+const http = require('http'); // For creating HTTP client
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Remember to use npm run dev to launch both back-end and front-end
+
+/* ----- Redirect to downloading MP3 Files from YouTube ---- */
+app.post('/download', (req, res) => {
+
+    // Gathering Information from React
+    let youtubeLinks = req.body.youtubeLinks;
+    let songInfo = req.body.playlist;
+
+    console.log("Number of Songs: " + youtubeLinks.length);
+
+    // If there aren't any YouTube links, there is an error
+    if (!youtubeLinks) {
+        return res.status(400).send({ error: 'No array was sent' });
+    }
+
+    // Creating the name of the file [Song - Artist(s)]
+    let fileNames = [];
+
+    for (let x = 0; x < songInfo.length; x++) {
+        let songName = songInfo[x].name;
+        let songArtists = [];
+
+        for (let i = 0; i < songInfo[x].artists.length; i++) {
+            songArtists.push(songInfo[x].artists[i].name);
+            console.log()
+        }
+
+        let artists = songArtists.join(', ');
+        let file = songName + ' - ' + artists;
+        fileNames.push(file);
+    }
+
+    // Adjusting Compression Level
+    const archive = archiver('zip', {
+        zlib: { level: 9 }
+    });
+
+    res.attachment('music.zip'); // Sets HTTP response and zip file name
+
+    // Information when archiver closes
+    archive.on('close', () => {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('Archiver has been finalized and the output file descriptor has closed');
+    });
+
+    // If archiver encounters an error
+    archive.on('error', (err) => {
+        throw err;
+    });
+
+    // Archiver sends to client
+    archive.pipe(res);
+
+    // YouTube Download Settings
+    const options = {
+        quality: 'highest',
+        filter: 'audioonly',
+        format: 'mp3'
+    }
+
+    console.log(fileNames);
+    console.log("To Download:")
+
+    // Adding YouTube MP3 files in the zip file
+    for (let i = 0; i < youtubeLinks.length; i++) {
+        let file = ytdl(youtubeLinks[i], options);
+        archive.append(file, { name: `${fileNames[i]}.mp3` });
+        console.log(fileNames[i]);
+        i++;
+    }
+
+    archive.finalize();
+});
+
+// Run Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`Server started on port ${ PORT }`));
